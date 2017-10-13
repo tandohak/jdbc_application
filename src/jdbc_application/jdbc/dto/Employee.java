f2bdbe71362bc2f48695b20eeb29f4819fafb0ac
S_YoonPyo <ypyo09@naver.com>
2017-10-13 14:09:21 +0900

    emloyee equals override

--- src/jdbc_application/jdbc/dto/Employee.java
+++ src/jdbc_application/jdbc/dto/Employee.java
@@ -19,6 +19,28 @@ public class Employee {
 
 
 
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + empno;
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employee other = (Employee) obj;
+		if (empno != other.empno)
+			return false;
+		return true;
+	}
+
 	public Employee() {
 		// TODO Auto-generated constructor stub
 	}
